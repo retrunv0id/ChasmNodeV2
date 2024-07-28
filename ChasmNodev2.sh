0889a16a7b817297bf05157d94c21c669df2d2f0
retrunvoid <45552788+retrunv0id@users.noreply.github.com>
2024-07-28 20:30:10 +0700

    Update ChasmNodev2.sh

--- ChasmNodev2.sh
+++ ChasmNodev2.sh
@@ -1,40 +1,49 @@
 #!/bin/bash
 
 # Banner welcome
-echo "Welcome ChasmNodeV2"
+echo "
+                                             _     _ 
+            _                               (_)   | |
+  ____ ____| |_   ____ _   _ ____ _   _ ___  _  _ | |
+ / ___) _  )  _) / ___) | | |  _ \ | | / _ \| |/ || |
+| |  ( (/ /| |__| |   | |_| | | | \ V / |_| | ( (_| |
+|_|   \____)\___)_|    \____|_| |_|\_/ \___/|_|\____|
+                                                     
+"
 
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt --fix-broken install -y
 sudo apt-get autoremove -y
 clear
 
-echo "Konfigurasi Awal Chasm Scout"
-echo "---------------------------"
-read -p "Masukkan Nama Chasm Scout: " SCOUTNAME
-read -p "Masukkan UID Scout: " SCOUTUID
-read -p "Masukkan Kunci API Webhook: " WEBHOOKAPI
-read -p "Masukkan Kunci API Groq: " GROQAPI
+echo "══════════════════════════════════"
+echo "     ══ Input Konfigurasi ══"
+echo "══════════════════════════════════"
+read -p "══■ ➡️Nama Chasm Scout: " SCOUTNAME
+read -p "══■ ➡️UID Scout: " SCOUTUID
+read -p "══■ ➡️API Webhook: " WEBHOOKAPI
+read -p "══■ ➡️API Groq: " GROQAPI
 
-echo "Membersihkan paket Docker lama..."
+echo "══ Hapus paket Docker lama"
 for paket in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do
     sudo apt-get remove -y $paket
 done
 
-echo "Instalasi paket pendukung Docker..."
+echo "══■ Paket Pendukung Docker ■══"
 sudo apt-get install -y ca-certificates curl
 
-echo "Menyiapkan kunci GPG dan repositori Docker..."
+echo "══■ kunci GPG Dan Repositori Docker ■══"
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-echo "Menambahkan repository Docker..."
+echo "══■ repository Docker ■══"
 sudo sh -c "echo 'deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable' > /etc/apt/sources.list.d/docker.list"
 
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-echo "Mengatur direktori Chasm..."
+echo "══■ direktori Chasm ■══"
 mkdir -p ~/chasm
 cd ~/chasm
 
@@ -51,11 +60,11 @@ MODEL=gemma2-9b-it
 GROQ_API_KEY=$GROQAPI
 EOF
 
-echo "Memulai Chasm Scout..."
+echo "══■ Start Chasm Scout ■══"
 ufw allow 3001
 docker pull chasmtech/chasm-scout:latest
 docker run -d --restart=always --env-file ./.env -p 3001:3001 --name scout chasmtech/chasm-scout
 echo "Chasm Scout sedang berjalan..."
 
-echo "Membersihkan..."
+echo "══■ Selesai Install Chasm Check On Dasboard ■══"
 rm -- "$0"
